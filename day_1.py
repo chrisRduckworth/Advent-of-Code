e9ca982a46b5bb2e13d17f84cd5ead8bc5be1e8d
chris <c_duckworth@hotmail.com>
2023-12-01 14:31:02 +0000

    add solution for part 2

--- day_1.py
+++ day_1.py
@@ -1,9 +1,16 @@
 import re
+import unicodedata
+
+def replace_digits(match):
+    numbers = {unicodedata.name(str(i))[6:].lower(): str(i) for i in range(1,10)}
+    return match.group(1) + numbers[match.group(2)] + match.group(2)[-1]
 
 def sum_calibration_values(calibration_values):
     calibration_values = calibration_values.splitlines()
     total = 0
     for value in calibration_values:
+        value = re.sub(r"^(.*?)(one|two|three|four|five|six|seven|eight|nine)", replace_digits, value)
+        value = re.sub(r"^(.*)(one|two|three|four|five|six|seven|eight|nine)", replace_digits, value)
         digits = re.match(r"^[^\d]*(\d)(.*(\d))?[^\d]*$", value)
         if digits[3] is not None:
             calibration_value = f"{digits[1]}{digits[3]}"
@@ -13,8 +20,8 @@ def sum_calibration_values(calibration_values):
         total += calibration_value
     return total
 
-with open("inputs/day_1.txt") as f:
-    calibration_values = f.read()
-    print(calibration_values)
-    total = sum_calibration_values(calibration_values)
-    print(total)
+if __name__ == "__main__":
+    with open("inputs/day_1.txt") as f:
+        calibration_values = f.read()
+        total = sum_calibration_values(calibration_values)
+        print(total)
