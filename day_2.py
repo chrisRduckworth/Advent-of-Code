3ae2a9c11e4aa6ff4b39e59b079a52e633fd38e9
chris <c_duckworth@hotmail.com>
2023-12-02 10:31:50 +0000

    add solution for multiple games

--- day_2.py
+++ day_2.py
@@ -1,14 +1,17 @@
 import re
 
 def sum_valid_games(games):
-    red_cubes = re.findall(r"\d+(?= red)", games)
-    green_cubes = re.findall(r"\d+(?= green)", games)
-    blue_cubes = re.findall(r"\d+(?= blue)", games)
-    red_is_valid = all(int(cubes) <= 12 for cubes in red_cubes)
-    green_is_valid = all(int(cubes) <= 13 for cubes in green_cubes)
-    blue_is_valid = all(int(cubes) <= 14 for cubes in blue_cubes)
-    if red_is_valid and green_is_valid and blue_is_valid:
-        return 1
-    else:
-        return 0
+    games = games.splitlines()
+    total = 0
+    for game in games:
+        game_id = re.search(r"(?<=Game )\d+", game)[0]
+        red_cubes = re.findall(r"\d+(?= red)", game)
+        green_cubes = re.findall(r"\d+(?= green)", game)
+        blue_cubes = re.findall(r"\d+(?= blue)", game)
+        red_is_valid = all(int(cubes) <= 12 for cubes in red_cubes)
+        green_is_valid = all(int(cubes) <= 13 for cubes in green_cubes)
+        blue_is_valid = all(int(cubes) <= 14 for cubes in blue_cubes)
+        if red_is_valid and green_is_valid and blue_is_valid:
+            total += int(game_id)
+    return total
 
