db225f7b582799b103fffd6f2cf7c8b45c9cb8fb
chris <c_duckworth@hotmail.com>
2023-12-02 10:44:28 +0000

    add basic tests for sum_minimum_powers

--- tests/day_2_test.py
+++ tests/day_2_test.py
@@ -1,4 +1,4 @@
-from day_2 import sum_valid_games
+from day_2 import sum_valid_games, sum_minimum_powers
 
 class TestSumValidGames:
     def test_returns_sum_of_single_valid_game(self):
@@ -41,3 +41,18 @@ class TestSumValidGames:
         sum_of_games = sum_valid_games(games)
 
         assert sum_of_games == 8
+
+class TestSumMinimumPowers:
+    def test_returns_sum_powers_of_single_game(self):
+        games = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
+        
+        sum_of_minimum_powers = sum_minimum_powers(games)
+
+        assert sum_of_minimum_powers == 48
+
+    def test_returns_sum_powers_of_multiple_games(self):
+        games = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\nGame 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\nGame 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\nGame 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\nGame 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
+
+        sum_of_minimum_powers = sum_minimum_powers(games)
+
+        assert sum_of_minimum_powers == 2286
