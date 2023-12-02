75594f45bcb987595f155cf3e9f228396c2dad7e
chris <c_duckworth@hotmail.com>
2023-12-02 10:31:34 +0000

    add tests for multiple games

--- tests/day_2_test.py
+++ tests/day_2_test.py
@@ -20,3 +20,24 @@ class TestSumValidGames:
         assert sum_of_games == 0
         assert sum_of_games_2 == 0
         assert sum_of_games_3 == 0
+
+    def test_returns_sum_of_multiple_valid_games(self):
+        games = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\nGame 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 bluen\nGame 3: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
+
+        sum_of_games = sum_valid_games(games)
+
+        assert sum_of_games == 6
+
+    def test_returns_sum_of_multiple_invalid_games(self):
+        games = "Game 1: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\nGame 2: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red"
+
+        sum_of_games = sum_valid_games(games)
+
+        assert sum_of_games == 0
+
+    def test_returns_sum_of_mixed_games(self):
+        games = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\nGame 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\nGame 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\nGame 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\nGame 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
+
+        sum_of_games = sum_valid_games(games)
+
+        assert sum_of_games == 8
