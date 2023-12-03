a374f4fe9d08efb19c8465195555bf940c3d5512
chris <c_duckworth@hotmail.com>
2023-12-03 13:24:13 +0000

    has_adjacent_symbols passes base case tests

--- day_3.py
+++ day_3.py
@@ -1,7 +1,13 @@
 import re
 
 def has_adjacent_symbol(start_index, length, rows):
-    pass
+    top_characters = rows[0][start_index - 1:start_index + length + 1]
+    bottom_characters = rows[2][start_index - 1:start_index + length + 1]
+    left_character = rows[1][start_index - 1]
+    right_character = rows[1][start_index + length]
+    adjacent_characters = top_characters + left_character + right_character + bottom_characters
+
+    return re.search(r"[^.]", adjacent_characters) is not None
 
 def sum_part_numbers(schematic):
     pass 
