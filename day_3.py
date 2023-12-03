e9c29b0abcd867ff109020a3e8e07114d35354ff
chris <c_duckworth@hotmail.com>
2023-12-03 23:19:07 +0000

    add sum_part_numbers

--- day_3.py
+++ day_3.py
@@ -20,4 +20,38 @@ def has_adjacent_symbol(start_index, length, rows):
     return re.search(r"[^.]", adjacent_characters) is not None
 
 def sum_part_numbers(schematic):
-    pass 
+    """finds the sum of all valid numbers in the shcematic"""
+    lines = schematic.splitlines()
+    total = 0
+    
+    # first row
+    first_row_numbers = re.finditer(r"\d+", lines[0])
+    for number in first_row_numbers:
+        start_index = number.start()
+        length = number.end() - number.start()
+        rows = ["." * len(lines[0]), lines[0], lines[1]]
+        if has_adjacent_symbol(start_index, length, rows):
+            total += int(number[0])
+    
+    # middle rows
+    for i in range(1, len(lines) - 1):
+        line = lines[i]
+        numbers = re.finditer(r"\d+", line)
+        for number in numbers:
+            start_index = number.start()
+            length = number.end() - number.start()
+            rows = lines[i - 1: i + 2]
+            if has_adjacent_symbol(start_index, length, rows):
+                total += int(number[0])
+    
+    # last row 
+    last_row_numbers = re.finditer(r"\d+", lines[-1])
+    for number in last_row_numbers:
+        start_index = number.start()
+        length = number.end() - number.start()
+        rows = [lines[-2], lines[-1], "." * len(lines[-1])]
+        if has_adjacent_symbol(start_index, length, rows):
+            total += int(number[0])
+
+    return total
+    
