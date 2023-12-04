c32a98678e2b4900700a541e366b5cbea580c898
chris <c_duckworth@hotmail.com>
2023-12-04 10:00:24 +0000

    complete sum_scratchcard_points

--- day_4.py
+++ day_4.py
@@ -1,4 +1,17 @@
 import re
 
 def sum_scratchcard_points(scratchcards):
-    pass
+    scratchcards = scratchcards.splitlines()
+    total = 0
+    for card in scratchcards:
+        winning_numbers, your_numbers = card.split("|")
+        winning_numbers = re.findall(r"\d+", winning_numbers)[1:]
+        your_numbers = re.findall(r"\d+", your_numbers)
+        matches = 0
+        for number in your_numbers:
+            if number in winning_numbers:
+                matches += 1 
+        if matches > 0:
+            total += 2 ** (matches - 1)
+
+    return total
