c3969f8a1ea95a06b3084d6e35bdaa02f7bcd52b
chris <c_duckworth@hotmail.com>
2023-12-04 12:40:02 +0000

    add comments

--- day_4.py
+++ day_4.py
@@ -1,6 +1,7 @@
 import re
 
 def sum_scratchcard_points(scratchcards):
+    """returns total number of scratchcard points"""
     scratchcards = scratchcards.splitlines()
     total = 0
     for card in scratchcards:
@@ -17,8 +18,10 @@ def sum_scratchcard_points(scratchcards):
     return total
 
 def find_total_scratchcards(scratchcards):
+    """returns the total number of scratchcards"""
     scratchcards = scratchcards.splitlines()
 
+    # start with one of every card
     scratchcard_count = {i: 1 for i in range(1, len(scratchcards) + 1)}
 
     for i, card in enumerate(scratchcards):
@@ -30,8 +33,14 @@ def find_total_scratchcards(scratchcards):
         for number in your_numbers:
             if number in winning_numbers:
                 matches += 1 
+
+        # each of your numbers that is a winner adds one to the count of
+        # the next cards
         for j in range(1, matches + 1):
             if card_number + j <= len(scratchcards):
+                # each card adds its own count to subsequent cards for the match
+                # eg if you have 3 card 1s, then each increases card 2 by 1,
+                # so the number of card 2s increases by 3
                 scratchcard_count[card_number + j] += scratchcard_count[card_number]
 
     return sum(scratchcard_count.values())
