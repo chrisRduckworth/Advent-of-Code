c47959e017695cbea2cd3d47427f5d6f3e5f0cc6
chris <c_duckworth@hotmail.com>
2023-12-05 16:53:36 +0000

    add test for range of seeds

--- tests/day_5_test.py
+++ tests/day_5_test.py
@@ -1,4 +1,4 @@
-from day_5 import find_lowest_location_number, find_new_value
+from day_5 import find_lowest_location_number, find_new_value, find_lowest_location_number_from_ranges
 
 test_input = """seeds: 79 14 55 13
 
@@ -50,6 +50,22 @@ class TestFindNewValue:
         out_value = find_new_value(map_values, in_value)
 
         assert out_value == 48
+
+    def test_swapping_map_columns_reverses_direction(self):
+        map_values = ["98 50 2", "50 52 48"]
+
+        in_value = 81
+
+        out_value = find_new_value(map_values, in_value)
+
+        assert out_value == 79
+
+        # no for a number not in the map
+        in_value = 48
+
+        out_value = find_new_value(map_values, in_value)
+
+        assert out_value == 48
         
 class TestFindLowestLocationNumber:
     def test_returns_lowest_location_number_for_one_seed(self):
@@ -65,3 +81,11 @@ class TestFindLowestLocationNumber:
         min_location = find_lowest_location_number(almanac)
 
         assert min_location == 35
+
+class TestFindLowestLocationNumberFromRanges:
+    def test_returns_lowest_location(self):
+        almanac = test_input
+
+        min_location = find_lowest_location_number_from_ranges(almanac)
+
+        assert min_location == 46
