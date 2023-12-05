72d742228d753fbf2f8a20e296913e14773c42aa
chris <c_duckworth@hotmail.com>
2023-12-05 11:24:12 +0000

    add test for single seed

---
+++ tests/day_5_test.py
@@ -0,0 +1,43 @@
+from day_5 import find_lowest_location_number
+
+test_input = """seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4"""
+
+class TestFindLowestLocationNumber:
+    def test_returns_lowest_location_number_for_one_seed(self):
+        almanac = "seeds: 79\n" + "\n".join(test_input.splitlines()[1:])
+
+        min_location = find_lowest_location_number(almanac)
+    
+        assert min_location == 82
