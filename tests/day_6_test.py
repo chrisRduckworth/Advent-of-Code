fd656589ad50cbafcb2c44fb6334f3ba5abc5718
chris <c_duckworth@hotmail.com>
2023-12-06 11:28:22 +0000

    add day 6 files

---
+++ tests/day_6_test.py
@@ -0,0 +1,8 @@
+from day_6 import find_winning_products
+
+class FindWinningProducts:
+    def test_returns_value_for_single_race(self):
+        pass
+
+    def test_returns_value_for_multiple_races(self):
+        pass
