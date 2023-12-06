5a1467c38513e1c0d829097614f7d1b93f227721
chris <c_duckworth@hotmail.com>
2023-12-06 12:19:27 +0000

    add solution for big race

--- day_6.py
+++ day_6.py
@@ -11,8 +11,19 @@ def find_winning_products(races):
     
     return product
 
+def find_winning_times(races):
+    time = int(races.splitlines()[0][5:].replace(" ", ""))
+    distance = int(races.splitlines()[1][9:].replace(" ", ""))
+
+    minimum_button_press = math.floor((time - math.sqrt(time**2 - 4 * distance))/2) + 1
+    max_button_press = math.ceil((time + math.sqrt(time**2 - 4 * distance))/2) - 1
+    
+    return max_button_press + 1 - minimum_button_press
+
 if __name__ == "__main__":
     with open("inputs/day_6.txt") as f:
         races = f.read()
         product = find_winning_products(races)
         print(product, " product of number of winning times")
+        winning_times = find_winning_times(races)
+        print(winning_times, " winning times for big race")
