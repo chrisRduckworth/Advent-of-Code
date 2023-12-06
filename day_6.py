150aefdea70e35d677a608fb3c936dbc46e3f524
chris <c_duckworth@hotmail.com>
2023-12-06 12:07:06 +0000

    complete find_winning_products

--- day_6.py
+++ day_6.py
@@ -1,2 +1,12 @@
+import re, math
+
 def find_winning_products(races):
-    pass
+    times, distances = [[int(n) for n in re.findall(r"\d+", l)] for l in races.splitlines()]
+    product = 1
+    for i in range(0, len(times)):
+        minimum_button_press = math.floor((times[i] - math.sqrt(times[i]**2 - 4 * distances[i]))/2) + 1
+        max_button_press = math.ceil((times[i] + math.sqrt(times[i]**2 - 4 * distances[i]))/2) - 1
+        # we have to include the +/- 1 at the end for the above to deal with integer solutions
+        product *= (max_button_press + 1 - minimum_button_press)
+    
+    return product
