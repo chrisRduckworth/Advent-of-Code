d29cfc2c9d507bf0465c6682585d0543a606d6b6
chris <c_duckworth@hotmail.com>
2023-12-06 12:06:49 +0000

    add tests for find_winning-products

--- tests/day_6_test.py
+++ tests/day_6_test.py
@@ -1,8 +1,16 @@
 from day_6 import find_winning_products
 
-class FindWinningProducts:
+class TestFindWinningProducts:
     def test_returns_value_for_single_race(self):
-        pass
+        races = "Time:      7\nDistance:  9"
+
+        winning_times = find_winning_products(races)
+
+        assert winning_times == 4
 
     def test_returns_value_for_multiple_races(self):
-        pass
+        races = "Time:      7  15   30\nDistance:  9  40  200"
+
+        product = find_winning_products(races)
+
+        assert product == 288
