c7aa4d44a3739f6bcb6d52e7666cd9c74f84594c
chris <c_duckworth@hotmail.com>
2023-12-07 17:28:13 +0000

    add convert_hand_to_number_with_jokers

--- day_7.py
+++ day_7.py
@@ -48,6 +48,41 @@ def total_winnings(games):
     winnings = sum((i + 1) * games[hand] for i, hand in enumerate(hands))
     return winnings
 
+def convert_hand_to_number_with_jokers(hand):
+    number_of_jokers = hand.count("J")
+    unique_cards = set(hand.replace("J", ""))
+    highest_number_of_single_card = max([hand.count(c) for c in unique_cards])
+    # now account for jokers 
+    highest_number_of_single_card += number_of_jokers
+
+    if highest_number_of_single_card == 5:
+        out_value = "7"
+    elif highest_number_of_single_card == 4:
+        out_value = "6"
+    elif highest_number_of_single_card == 3:
+        if len(unique_cards) == 2:
+            # full house
+            out_value = "5"
+        else:
+            # 3 of a kind
+            out_value = "4"
+    elif highest_number_of_single_card == 2:
+        if len(unique_cards) == 3:
+            # 2 pair
+            out_value = "3"
+        else:
+            # 1 pair
+            out_value = "2"
+    else:
+        out_value = "1"
+
+    card_hex_values = {"2": "2", "3": "3", "4": "4", "5": "5", "6": "6", "7": "7", "8": "8", "9":"9", "T": "A", "J": "1", "Q": "C", "K": "D", "A": "E"}
+
+    for card in hand:
+        out_value += card_hex_values[card]
+
+    return int(out_value, 16)
+
 if __name__ == "__main__":
     with open("inputs/day_7.txt") as f:
         games = f.read()
