274d29aa9264e97cafe4c84653aaf411af93b5e1
chris <c_duckworth@hotmail.com>
2023-12-07 16:06:40 +0000

    add files for day 7

---
+++ day_7.py
@@ -0,0 +1,6 @@
+def convert_hand_to_number(hand):
+    """converts a hand to an integer so it can be compared in a sort"""
+    pass
+
+def total_winnings(hands):
+    pass
---
+++ tests/day_7_test.py
@@ -0,0 +1,8 @@
+from day_7 import convert_hand_to_number, total_winnings
+
+class TestConvertHandToNumber:
+    def returns_a_number(self):
+        hand = "K43AT"
+
+        assert isintsnace("K43AT", int)
+
