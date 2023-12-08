4bfe022a31c04e92a56cb6588ff3e3275b86d5b7
chris <c_duckworth@hotmail.com>
2023-12-08 11:28:37 +0000

    add find_steps

--- day_8.py
+++ day_8.py
@@ -1,3 +1,20 @@
+import re
+
 def find_steps(maps):
-    pass
+    maps = maps.splitlines()
+    directions = maps[0]
+    network = {l[:3]: re.findall(r"\w+", l[3:]) for l in maps[2:]}
+
+    location = "AAA"
+    i = 0
+
+    while location != "ZZZ":
+        direction = directions[i % len(directions)]
+        if direction == "L":
+            location = network[location][0]
+        else:
+            location = network[location][1]
+        i += 1
+        
+    return i
 
