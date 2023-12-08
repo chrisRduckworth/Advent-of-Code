f4a09ac1c7bf966845e11ecb613011a83d790a53
chris <c_duckworth@hotmail.com>
2023-12-08 12:01:20 +0000

    add find_ghost_steps

--- day_8.py
+++ day_8.py
@@ -18,6 +18,26 @@ def find_steps(maps):
         
     return i
 
+def find_ghost_steps(maps):
+    maps = maps.splitlines()
+    directions = maps[0]
+    network = {l[:3]: re.findall(r"\w+", l[3:]) for l in maps[2:]}
+
+    locations = [loc for loc in network if loc[2] == "A"]
+    i = 0
+
+    while not all(loc[2] == "Z" for loc in locations):
+        direction = directions[i % len(directions)]
+        for index, location in enumerate(locations):
+            if direction == "L":
+                location = network[location][0]
+            else:
+                location = network[location][1]
+            locations[index] = location
+        i += 1
+        
+    return i
+
 if __name__ == "__main__":
     with open("inputs/day_8.txt") as f:
         maps = f.read()
