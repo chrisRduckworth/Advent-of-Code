201823d59001be589e1789be725928c5a71bd343
chris <c_duckworth@hotmail.com>
2023-12-08 11:26:13 +0000

    add tests for find_steps

--- tests/day_8_test.py
+++ tests/day_8_test.py
@@ -1,11 +1,36 @@
 from day_8 import find_steps
 
 class TestFindSteps:
-    def returns_1_for_simple_case(self):
-        pass
+    def test_returns_1_for_simple_case(self):
+        maps = "L\n\nAAA = (ZZZ, AAA)\nZZZ = (ZZZ, ZZZ)"
 
-    def returns_steps_for_long_directions(self):
-        pass
+        steps = find_steps(maps)
 
-    def returns_steps_for_short_directions(self):
-        pass
+        assert steps == 1
+
+    def test_returns_steps_for_long_directions(self):
+        maps = """RL
+
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)"""
+
+        steps = find_steps(maps)
+
+        assert steps == 2
+
+    def test_returns_steps_for_short_directions(self):
+        maps = """LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)"""
+
+        steps = find_steps(maps)
+
+        assert steps == 6
+        
