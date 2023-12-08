8f3d4ccb01e9b036d43cee1924097fbd6ddbbc0a
chris <c_duckworth@hotmail.com>
2023-12-08 11:11:46 +0000

    create files for day 8

---
+++ day_8.py
@@ -0,0 +1,3 @@
+def find_steps(maps):
+    pass
+
---
+++ tests/day_8_test.py
@@ -0,0 +1,11 @@
+from day_8 import find_steps
+
+class TestFindSteps:
+    def returns_1_for_simple_case(self):
+        pass
+
+    def returns_steps_for_long_directions(self):
+        pass
+
+    def returns_steps_for_short_directions(self):
+        pass
