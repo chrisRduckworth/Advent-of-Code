dcc9770aa762b83fb7baf022efc5832b99a806c9
chris <c_duckworth@hotmail.com>
2023-12-08 12:01:29 +0000

    add tests for find ghost steps

--- tests/day_8_test.py
+++ tests/day_8_test.py
@@ -1,4 +1,4 @@
-from day_8 import find_steps
+from day_8 import find_steps, find_ghost_steps
 
 class TestFindSteps:
     def test_returns_1_for_simple_case(self):
@@ -34,3 +34,42 @@ ZZZ = (ZZZ, ZZZ)"""
 
         assert steps == 6
         
+class TestFindGhostSteps:
+    def test_returns_1_for_simple_case(self):
+        maps = "L\n\nAAA = (ZZZ, AAA)\nZZZ = (ZZZ, ZZZ)"
+
+        steps = find_ghost_steps(maps)
+
+        assert steps == 1
+    
+    def test_returns_steps_for_long_directions(self):
+        maps =   """LRLRLRLRLR
+
+AAA = (AAB, XXX)
+AAB = (XXX, AAZ)
+AAZ = (AAB, XXX)
+BBA = (BBB, XXX)
+BBB = (BBC, BBC)
+BBC = (BBZ, BBZ)
+BBZ = (BBB, BBB)
+XXX = (XXX, XXX)"""  
+        
+        steps = find_ghost_steps(maps)
+
+        assert steps == 6
+    
+    def test_returns_steps_for_short_directions(self):
+        maps =   """LR
+
+AAA = (AAB, XXX)
+AAB = (XXX, AAZ)
+AAZ = (AAB, XXX)
+BBA = (BBB, XXX)
+BBB = (BBC, BBC)
+BBC = (BBZ, BBZ)
+BBZ = (BBB, BBB)
+XXX = (XXX, XXX)"""  
+        
+        steps = find_ghost_steps(maps)
+
+        assert steps == 6
