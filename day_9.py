e65b4e72b0cfb718c3420c142d44a6e1f17c6f5f
chris <c_duckworth@hotmail.com>
2023-12-09 12:50:10 +0000

    complete find_subsequences

--- day_9.py
+++ day_9.py
@@ -1,5 +1,8 @@
 def find_subsequences(sequence):
-    pass
+    if all(n == sequence[0] for n in sequence):
+        return [sequence]
+    differences = [sequence[i + 1] - sequence[i] for i in range(0, len(sequence) - 1)]
+    return [sequence, *find_subsequences(differences)]
 
-def sum_extrapolated_values(report)
+def sum_extrapolated_values(report):
     pass
