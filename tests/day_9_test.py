0167978700d45f8b4e07d96d236df23c17cbaa73
chris <c_duckworth@hotmail.com>
2023-12-09 13:04:39 +0000

    add tests for find_next_value

--- tests/day_9_test.py
+++ tests/day_9_test.py
@@ -1,4 +1,4 @@
-from day_9 import find_subsequences, sum_extrapolated_values
+from day_9 import find_subsequences, find_next_value, sum_extrapolated_values
 
 class TestFindSubsequences:
     def test_returns_sequence_for_constant(self):
@@ -33,3 +33,38 @@ class TestFindSubsequences:
                 [0, 2, 4, 6],
                 [2, 2, 2]
                 ]
+
+class TestFindNextValue:
+    def test_returns_number_for_constant(self):
+        subsequences = [[3, 3, 3, 3]]
+
+        next_value = find_next_value(subsequences)
+
+        assert next_value == 3
+    
+    def test_returns_next_number_for_linear(self):
+        subsequences = [[0, 3, 6, 9, 12, 15], [3, 3, 3, 3, 3]]
+
+        next_value = find_next_value(subsequences)
+
+        assert next_value == 18
+
+    def test_returns_next_number(self):
+        subsequences_1 = [
+            [1, 3, 6, 10, 15, 21],
+            [2, 3, 4, 5, 6],
+            [1, 1, 1, 1]
+            ]
+
+        subsequences_2 = [
+            [10, 13, 16, 21, 30, 45],
+            [3, 3, 5, 9, 15],
+            [0, 2, 4, 6],
+            [2, 2, 2]
+        ]
+
+        next_value_1 = find_next_value(subsequences_1)
+        next_value_2 = find_next_value(subsequences_2)
+
+        assert next_value_1 == 28
+        assert next_value_2 == 68
