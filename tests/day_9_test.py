98b121ae5f250ea51d6b4bad0a33daf78bd532f6
chris <c_duckworth@hotmail.com>
2023-12-09 12:49:48 +0000

    fix linear test

--- tests/day_9_test.py
+++ tests/day_9_test.py
@@ -2,18 +2,18 @@ from day_9 import find_subsequences, sum_extrapolated_values
 
 class TestFindSubsequences:
     def test_returns_sequence_for_constant(self):
-        sequence = [3,3,3,3]
+        sequence = [3, 3, 3, 3]
 
         subsequences = find_subsequences(sequence)
 
-        assert subsequences == [[3,3,3,3]]
+        assert subsequences == [[3, 3, 3, 3]]
 
     def test_returns_subsequences_for_linear(self):
-        sequence = [0,3,6,9,12,15]
+        sequence = [0, 3, 6, 9, 12, 15]
 
         subsequences = find_subsequences(sequence)
 
-        assert subsequences == [[0,3,6,9,12,15], [3,3,3,3]]
+        assert subsequences == [[0, 3, 6, 9, 12, 15], [3, 3, 3, 3, 3]]
 
     def test_returns_subsequences(self):
         sequence_1 = [1, 3, 6, 10, 15, 21]
