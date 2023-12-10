5e4b0f1a2a66ed1782e20cddcadf6b274ee9c1b7
chris <c_duckworth@hotmail.com>
2023-12-10 14:49:05 +0000

    add increment position

--- day_10.py
+++ day_10.py
@@ -1,6 +1,31 @@
 def increment_position(pipe, direction):
     """returns an increment for the coords"""
-    pass
+    if pipe not in "|-LJ7F":
+        raise ValueError("input must be pipe")
+    if pipe == "|":
+        if direction not in "NS":
+            raise ValueError("invalid direction")
+        return (0, 1) if direction == "N" else (0, -1)
+    if pipe == "-":
+        if direction not in "EW":
+            raise ValueError("invalid direction")
+        return (1, 0) if direction == "W" else (-1,0)
+    if pipe == "L":
+        if direction not in "NE":
+            raise ValueError("invalid direction")
+        return (1, 0) if direction == "N" else (0, -1)
+    if pipe == "J":
+        if direction not in "NW":
+            raise ValueError("invalid direction")
+        return (-1, 0) if direction == "N" else (0, -1)
+    if pipe == "7":
+        if direction not in "SW":
+            raise ValueError("invalid direction")
+        return (0, 1) if direction == "W" else (-1, 0)
+    if pipe == "F":
+        if direction not in "SE":
+            raise ValueError("invalid direction")
+        return (0, 1) if direction == "E" else (1, 0)
 
 def furthest_point(maze):
     pass
