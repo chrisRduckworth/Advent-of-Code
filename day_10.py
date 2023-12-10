59da1c984fec88b169db28cd92a2e7b365cae902
chris <c_duckworth@hotmail.com>
2023-12-10 16:54:36 +0000

    add input reading

--- day_10.py
+++ day_10.py
@@ -36,12 +36,17 @@ def find_start_coordinates(maze):
 def furthest_point(maze):
     maze = maze.splitlines()
     start_position = find_start_coordinates(maze)
+    movement_directions = {(0,1): "N", (0, -1): "S", (1, 0): "W", (-1, 0): "E"}
     # for my input start connects to south and west. We choose south
     movement = (0, 1)
     route = [start_position, (start_position[0], start_position[1] + 1)]
     while maze[route[-1][1]][route[-1][0]] != "S":
-        movement_directions = {(0,1): "N", (0, -1): "S", (1, 0): "W", (-1, 0): "E"}
         movement = increment_position(maze[route[-1][1]][route[-1][0]], movement_directions[movement])
         route.append((route[-1][0] + movement[0], route[-1][1] + movement[1]))
     return len(route[:-1]) / 2
 
+if __name__ == "__main__":
+    with open("inputs/day_10.txt") as f:
+        maze = f.read()
+        distance_to_furthest_point = furthest_point(maze)
+        print(distance_to_furthest_point, "< distance to furthest point")
