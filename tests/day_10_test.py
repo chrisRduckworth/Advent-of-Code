ab7c06bbed8ae36b713427a7c37c3213ec152010
chris <c_duckworth@hotmail.com>
2023-12-10 18:54:35 +0000

    add tests for returns number of enclosed tiles

--- tests/day_10_test.py
+++ tests/day_10_test.py
@@ -114,3 +114,40 @@ class TestMarkAdjacentTiles:
 
         assert maze == ["...", ".-.", "..."]
 
+class TestEnclosedTiles:
+    def test_returns_number_of_enclosed_tiles(self):
+        maze = """...........
+.S-------7.
+.|F-----7|.
+.||.....||.
+.||.....||.
+.|L-7.F-J|.
+.|..|.|..|.
+.L--J.L--J.
+..........."""
+
+        maze_2 = """.F----7F7F7F7F-7....
+.|F--7||||||||FJ....
+.||.FJ||||||||L7....
+FJL7L7LJLJ||LJ.L-7..
+L--J.L7...LJS7F-7L7.
+....F-J..F7FJ|L7L7L7
+....L7.F7||L7|.L7L7|
+.....|FJLJ|FJ|F7|.LJ
+....FJL-7.||.||||...
+....L---J.LJ.LJLJ..."""
+
+        maze_3 = """FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJ7F7FJ-
+L---JF-JLJ.||-FJLJJ7
+|F|F-JF---7F7-L7L|7|
+|FFJF7L7F-JF7|JL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L"""
+        
+        assert enclosed_tiles(maze, (0,1)) == 4
+        assert enclosed_tiles(maze_2, (0,1)) == 8
+        assert enclosed_tiles(maze_3, (0,1)) == 10
