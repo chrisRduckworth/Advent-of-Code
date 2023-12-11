0e30b89ff8382c5ac5336731dacc92fe499c9978
chris <c_duckworth@hotmail.com>
2023-12-11 13:35:12 +0000

    add input reading

--- day_11.py
+++ day_11.py
@@ -23,3 +23,9 @@ def sum_shortest_distances(image):
             total_distance += abs(galaxy[0] - end_galaxy[0]) + abs(galaxy[1] - end_galaxy[1])
 
     return total_distance
+
+if __name__ == "__main__":
+    with open("inputs/day_11.txt") as f:
+        image = f.read()
+        distance = sum_shortest_distances(image)
+        print(distance, "< sum of shortest distances")
