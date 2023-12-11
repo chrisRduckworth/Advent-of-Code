0bf77dbeaae0cabe42312fad54b9da413cfe20d2
chris <c_duckworth@hotmail.com>
2023-12-11 18:00:58 +0000

    add comments

--- day_11.py
+++ day_11.py
@@ -1,4 +1,5 @@
 def expand_space(image, char="."):
+    """adds empty lines for each row/column with no galaxy"""
     for i in range(len(image) - 1, -1, -1):
         if image[i] == "." * len(image[0]):
             image.insert(i, char * len(image[0]))
@@ -8,24 +9,35 @@ def expand_space(image, char="."):
     return image
 
 def find_distance(image, space_size, galaxy_1, galaxy_2):
+    """finds the distance between 2 galaxies"""
     change_x = abs(galaxy_1[0] - galaxy_2[0])
     change_y = abs(galaxy_1[1] - galaxy_2[1])
 
     start_galaxy_x = min(galaxy_1, galaxy_2, key=lambda x: x[0])
     start_galaxy_y = min(galaxy_1, galaxy_2, key=lambda x: x[1])
 
+    # first find path along x-axis
     x_string = image[start_galaxy_x[1]][start_galaxy_x[0] + 1: start_galaxy_x[0] + change_x]
+
+    # then path along y axis
     rows = image[start_galaxy_y[1]: start_galaxy_y[1] + change_y + 1]
     y_string = "".join(row[start_galaxy_x[0] + change_x] for row in rows)
+    
     shortest_path = x_string + y_string
+
+    # use space_size - 1 because we're already including it once in the 
+    # len(shortest_path)
     if change_x == 0:
+        # because otherwise you count the start twice
         return len(shortest_path) - 1 + (space_size - 1) * (shortest_path.count("@"))
     return len(shortest_path) + (space_size - 1) * (shortest_path.count("@"))
 
 def sum_shortest_distances(image, space_size = 1):
+    """finds the sum of the shortest distances between all galaxies"""
     image = image.splitlines()
     image = expand_space(image, "@")
 
+    # create a list of all galaxies
     galaxies = []
     for y, row in enumerate(image):
         for x, char in enumerate(row):
@@ -34,6 +46,8 @@ def sum_shortest_distances(image, space_size = 1):
 
     total_distance = 0
     for i, galaxy in enumerate(galaxies):
+        # for each galaxy, calculate distance from itself to each galaxy
+        # after itself in the list
         for end_galaxy in galaxies[i+1:]:
             total_distance += find_distance(image, space_size, galaxy, end_galaxy)
 
