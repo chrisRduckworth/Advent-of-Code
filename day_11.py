e3063db408f04bef2c86f02f0198113f620272eb
chris <c_duckworth@hotmail.com>
2023-12-11 13:22:16 +0000

    add expand_space function

--- day_11.py
+++ day_11.py
@@ -1,5 +1,11 @@
 def expand_space(image):
-    pass
+    for i in range(len(image) - 1, -1, -1):
+        if image[i] == "." * len(image[0]):
+            image.insert(i, "." * len(image[0]))
+    for i in range(len(image[0]) - 1, -1, -1):
+        if all(row[i] == "." for row in image):
+            image = [row[:i] + "." + row[i:] for row in image]
+    return image
 
 def sum_shortest_distances(image):
     image = image.splitlines()
