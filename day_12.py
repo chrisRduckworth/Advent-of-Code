3355df59535f4479a0d0b0e136e22522afcaa779
chris <c_duckworth@hotmail.com>
2023-12-12 11:38:48 +0000

    add create all springs function

--- day_12.py
+++ day_12.py
@@ -1,5 +1,16 @@
-def clean_ends(row, broken):
+from itertools import product
+
+def create_all_springs(length):
+    """returns all possible springs of a given length"""
+    return ["".join(comb) for comb in product(".#", repeat=length)]
+
+def filter_matching_springs(spring, possible_springs):
+    """returns springs which match the . and ? of the input spring"""
     pass
 
 def sum_possibilities(springs):
+    """sums the possible number of spring combinations"""
+    # instead of actually calculating how many possibilities for each,
+    # lets just make a bit list of all possible combinations 
+    # and filter them
     pass
