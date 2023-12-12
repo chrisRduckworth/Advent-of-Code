a854a65fa341e3e1299e8da7739ead9b8f1d4445
chris <c_duckworth@hotmail.com>
2023-12-12 11:47:22 +0000

    add tests for filter matching springs

--- tests/day_12_test.py
+++ tests/day_12_test.py
@@ -14,4 +14,24 @@ class TestCreateAllSprings:
 
         assert len(create_all_springs(length)) == 2 ** length
 
-    
+class TestFilterMatchingSprings:
+    def test_returns_spring_if_no_unknowns(self):
+        possible_springs = create_all_springs(2)
+        spring = ".."
+
+        assert filter_matching_springs(spring, possible_springs) == [".."]
+
+    def test_returns_all_springs_if_all_unknowns(self):
+        possible_springs = create_all_springs(2)
+        spring = "??"
+
+        assert filter_matching_springs(spring, possible_springs) == possible_springs
+
+    def test_returns_springs_that_match(self):
+        possible_springs = create_all_springs(3)
+        spring = "?.?"
+
+        matching_springs = filter_matching_springs(spring, possible_springs)
+
+        assert len(matching_springs) == 4
+        assert all(s[1] == "." for s in matching_springs)
