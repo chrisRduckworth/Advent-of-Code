653e4bceed3af14fd3e44c320262a4d20377dc13
chris <c_duckworth@hotmail.com>
2023-12-13 15:47:01 +0000

    add make notes function

--- day_13.py
+++ day_13.py
@@ -3,8 +3,8 @@ import numpy as np
 def compare_mirrored(pattern, k, column_or_row):
     """compares a mirrored version around the k-th column/row"""
     length = len(pattern) if column_or_row == "row" else len(pattern[0])
-    start_index = max(0, - len(pattern) + 2 * k)
-    end_index = min(len(pattern), 2 * k)
+    start_index = max(0, - length + 2 * k)
+    end_index = min(length, 2 * k)
     if column_or_row == "row":
         slice_to_compare = pattern[start_index: end_index]
         mirrored_slice = np.flipud(np.copy(slice_to_compare))
@@ -12,3 +12,18 @@ def compare_mirrored(pattern, k, column_or_row):
         slice_to_compare = [row[start_index:end_index] for row in pattern]
         mirrored_slice = np.fliplr(np.copy(slice_to_compare))
     return np.array_equal(slice_to_compare, mirrored_slice)
+
+def make_notes(pattern):
+    """sums rows and columns with symmetry"""
+    # columns
+    total = 0
+    for i in range(1, len(pattern[0])):
+        if compare_mirrored(pattern, i, "column"):
+            total += i
+    
+    # rows
+    for i in range(1, len(pattern)):
+        if compare_mirrored(pattern, i, "row"):
+            total += i * 100
+    return total
+
