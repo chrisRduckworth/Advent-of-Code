2d51a2d7c3b14425fe0eff4ff70c39e875de8cbd
chris <c_duckworth@hotmail.com>
2023-12-13 15:47:13 +0000

    add tests for make notes

--- tests/day_13_test.py
+++ tests/day_13_test.py
@@ -1,15 +1,15 @@
-from day_13 import compare_mirrored
+from day_13 import compare_mirrored, make_notes
 
 class TestCompareMirrored:
     def test_returns_true_for_symmetrical(self):
-        pattern = "#..\n#.."
+        pattern = [["#",".","."],["#",".","."]]
         k = 1
 
-        assert compare_mirrored(pattern, k, "column") == True
-        
-        pattern = "##\n..\n.."
-        
         assert compare_mirrored(pattern, k, "row") == True
+        
+        pattern = [["#","#"], [".", "."], [".","."]] 
+        
+        assert compare_mirrored(pattern, k, "column") == True
 
     def test_returns_true_if_pattern_mirrors_around_column_k(self):
         pattern = """#.##..##.
@@ -19,7 +19,7 @@ class TestCompareMirrored:
 ..#.##.#.
 ..##..##.
 #.#.##.#.""".splitlines()
-        print(pattern)
+        
         pattern = [list(row) for row in pattern]
         k = 5
 
@@ -64,3 +64,48 @@ class TestCompareMirrored:
         k = 6
 
         assert compare_mirrored(pattern, k, "row") == False
+
+class TestMakeNotes:
+    def test_returns_0_if_no_symmetry(self):
+        pattern = [[".","#"], ["#","."]]
+
+        notes = make_notes(pattern)
+
+        assert notes == 0
+
+    def test_adds_one_for_each_symmetrical_column(self):
+        pattern = """#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.""".splitlines()
+        pattern = [list(row) for row in pattern]
+
+        notes = make_notes(pattern)
+
+        assert notes == 5
+
+    def test_adds_100_for_each_symmetrical_row(self):
+        pattern = """#...##..#
+#....#..#
+..##..###
+#####.##.
+#####.##.
+..##..###
+#....#..#""".splitlines()
+        pattern = [list(row) for row in pattern]
+
+        notes = make_notes(pattern)
+
+        assert notes == 400 
+    
+    def test_adds_columns_and_rows(self):
+        pattern = """....\n....\n....\n####\n####""".splitlines()
+
+        pattern = [list(row) for row in pattern]
+
+        notes = make_notes(pattern)
+
+        assert notes == 506 
