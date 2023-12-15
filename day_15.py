262cc60b8b477ab1726fab2f2bf980f1869d0233
chris <c_duckworth@hotmail.com>
2023-12-15 14:44:03 +0000

    add dash function

--- day_15.py
+++ day_15.py
@@ -11,6 +11,20 @@ def calc_value(step):
     total = reduce(calc_new_value, step, 0)
     return total
 
+def dash(labels, boxes, step):
+    """removes the lens from labels and boxes"""
+    label = step[:-1]
+    box_number = calc_value(label)
+    if label not in labels:
+        return labels, boxes
+    
+    del labels[label]
+    
+    i = boxes[box_number].index(label)
+    del boxes[box_number][i]
+    
+    return labels, boxes
+
 if __name__ == "__main__":
     with open("inputs/day_15.txt") as f:
         sequence = f.read().split(",")
