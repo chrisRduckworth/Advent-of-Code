aacc2b4b8d937624bffd4dfc1f218c3011eef223
chris <c_duckworth@hotmail.com>
2023-12-17 11:52:28 +0000

    add day 17

---
+++ day_17.py
@@ -0,0 +1,2 @@
+def find_minimum_heat_loss(heat_map):
+    pass
---
+++ tests/day_17_test.py
@@ -0,0 +1,20 @@
+from day_17 import find_minimum_heat_loss
+
+class TestFindMinimumHeatLoss:
+    def test_returns_min_heat(self):
+        heat_map = """2413432311323
+3215453535623
+3255245654254
+3446585845452
+4546657867536
+1438598798454
+4457876987766
+3637877979653
+4654967986887
+4564679986453
+1224686865563
+2546548887735
+4322674655533""".splitlines()
+        heat_map = [[int(x) for x in list(r)] for r in heat_map]
+
+        assert find_minimum_heat_loss(heat_map) == 102
