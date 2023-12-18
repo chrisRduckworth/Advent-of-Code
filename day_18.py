3ee3e781b9f7a3f5da18d8000ed07802c6debae2
chris <c_duckworth@hotmail.com>
2023-12-18 14:09:38 +0000

    add files for day 18

---
+++ day_18.py
@@ -0,0 +1,12 @@
+def dig_trench(plan):
+    """creates an array representing the dug trench"""
+    # start with a 1x1 grid with a #
+    # then for each step:
+    # for each dig number:
+    # add a hash in that direction, 
+    # then an empty column/row in that direction
+    
+    # will also need to keep track of position
+    pass
+
+# flood fill for filling it up
---
+++ tests/day_18_test.py
@@ -0,0 +1,5 @@
+from day_18 import dig_trench
+
+class TestDigTrench:
+    def test_returns_digs_single_instruction(self):
+        pass
