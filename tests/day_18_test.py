6dbd6edf7f08126813724498c9ca943203c14a06
chris <c_duckworth@hotmail.com>
2023-12-18 14:34:10 +0000

    add test for basic instruction

--- tests/day_18_test.py
+++ tests/day_18_test.py
@@ -2,4 +2,26 @@ from day_18 import dig_trench
 
 class TestDigTrench:
     def test_returns_digs_single_instruction(self):
-        pass
+        instructions = [["R", 6, "(#70c710)"]]
+
+        trenches = dig_trench(instructions)
+
+        assert trenches == [["#","#","#","#","#","#","#"]]
+
+        instructions = [["D", 5, "(#0dc571)"]]
+
+        trenches = dig_trench(instructions)
+
+        assert trenches == [["#"],["#"],["#"],["#"],["#"],["#"]]
+
+        instructions = [["L", 2, "(#5713f0)"]]
+        
+        trenches = dig_trench(instructions)
+
+        assert trenches == [["#","#","#"]]
+
+        instructions = [["U", 3, "(#a77fa3)"]]
+        
+        trenches = dig_trench(instructions)
+
+        assert trenches == [["#"],["#"],["#"],["#"],]
