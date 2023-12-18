279b8f5486149627e00f6de7d49c1a0b720d53d8
chris <c_duckworth@hotmail.com>
2023-12-18 14:34:20 +0000

    add basic instruction handling

--- day_18.py
+++ day_18.py
@@ -1,4 +1,4 @@
-def dig_trench(plan):
+def dig_trench(instructions):
     """creates an array representing the dug trench"""
     # start with a 1x1 grid with a #
     # then for each step:
@@ -7,6 +7,34 @@ def dig_trench(plan):
     # then an empty column/row in that direction
     
     # will also need to keep track of position
-    pass
+    trenches = [["#"]]
+    position = (0,0)
+
+    for instruction in instructions:
+        for i in range(instruction[1]):
+            if instruction[0] == "R":
+                if position[0] + 1 == len(trenches[0]):
+                    trenches = [[*row, "."] for row in trenches]
+                position = (position[0] + 1, position[1])
+                trenches[position[1]][position[0]] = "#"
+
+            elif instruction[0] == "D":
+                if position[1] + 1 == len(trenches):
+                    trenches.append(["." for c in range(len(trenches[0]))])
+                position = (position[0], position[1] + 1)
+                trenches[position[1]][position[0]] = "#"
+
+            elif instruction[0] == "L":
+                if position[0] == 0:
+                    trenches = [[".", *row] for row in trenches]
+                trenches[position[1]][position[0]] = "#"
+
+            elif instruction[0] == "U":
+                if position[1] == 0:
+                    trenches.insert(0, ["." for c in range(len(trenches[0]))])
+                trenches[position[1]][position[0]] = "#"
+
+            
+    return trenches
 
 # flood fill for filling it up
