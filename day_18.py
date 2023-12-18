27cfcb2931b8149654dedcfa1c5885ac487a682b
chris <c_duckworth@hotmail.com>
2023-12-18 14:42:07 +0000

    add handling of multiple instructions

--- day_18.py
+++ day_18.py
@@ -1,12 +1,5 @@
 def dig_trench(instructions):
     """creates an array representing the dug trench"""
-    # start with a 1x1 grid with a #
-    # then for each step:
-    # for each dig number:
-    # add a hash in that direction, 
-    # then an empty column/row in that direction
-    
-    # will also need to keep track of position
     trenches = [["#"]]
     position = (0,0)
 
@@ -27,14 +20,17 @@ def dig_trench(instructions):
             elif instruction[0] == "L":
                 if position[0] == 0:
                     trenches = [[".", *row] for row in trenches]
+                else:
+                    position = (position[0] - 1, position[1])
                 trenches[position[1]][position[0]] = "#"
 
             elif instruction[0] == "U":
                 if position[1] == 0:
                     trenches.insert(0, ["." for c in range(len(trenches[0]))])
+                else:
+                    position = (position[0], position[1] - 1)
                 trenches[position[1]][position[0]] = "#"
 
-            
     return trenches
 
 # flood fill for filling it up
