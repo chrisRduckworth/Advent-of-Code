4db6d326d1e4c130496d481ee1f30b123e9693b9
chris <c_duckworth@hotmail.com>
2023-12-18 18:24:47 +0000

    refactor to use heapq

--- day_17.py
+++ day_17.py
@@ -1,106 +1,106 @@
 import math
+from heapq import *
 
 def find_minimum_heat_loss(heat_map):
     """Ooooh, this is my first time using Dijkstra"""
-    # pad all sides with an inifinte value. This just
-    # makes indexing easier
-    heat_map.insert(0, [math.inf for n in range(len(heat_map[0]))])
-    heat_map.append([math.inf for n in range(len(heat_map[0]))])
-    heat_map = [[math.inf, *r, math.inf] for r in heat_map]
-
     # dictionary keys need to include position,
     # direction,
     # and number of steps in a straight line to get there
-    unvisited = {}
+    result_map = {}
     for x in range(len(heat_map[0])):
         for y in range(len(heat_map)):
             for direction in "NESW":
                 for steps in range(1,4):
-                    unvisited[((x,y), direction, steps)] = heat_map[1][1] if x == y == 1 else math.inf
+                    result_map[((x,y), direction, steps)] = math.inf
 
-    visited = {}
-    current_node = ((1,1), "E", 1)
+    result_map[((0,0), "E", 1)] = 0
+    result_map[((0,0), "S", 1)] = 0
+    
+    visited = set()
+    
+    queue = []
+    for n in result_map:
+        heappush(queue, (result_map[n], n))
+    
     direction_coords = {"N": (0,-1), "E": (1,0), "S":(0,1), "W":(-1,0)}
     directions = {"N": "NEW", "E": "NES", "S": "ESW", "W": "NSW"}
     
-    while current_node[0] != (len(heat_map[0]) - 2, len(heat_map) - 2):
+    while queue:
         # find adjacent nodes
+        current_distance, current_node = heappop(queue)
+        
         adjacent_nodes = []
         for direction in directions[current_node[1]]:
             x = current_node[0][0] + direction_coords[direction][0]
             y = current_node[0][1] + direction_coords[direction][1]
             if current_node[1] != direction:
                 node = ((x,y), direction, 1)
-                if node in unvisited:
+                if node not in visited and node in result_map:
                     adjacent_nodes.append(node)
             elif current_node[2] < 3:
                 node = ((x,y), direction, current_node[2] + 1)
-                if node in unvisited:
+                if node not in visited and node in result_map:
                     adjacent_nodes.append(node)
 
         for node in adjacent_nodes:
             x, y = node[0]
-            distance = unvisited[current_node] + heat_map[y][x]
-            if distance < unvisited[node]:
-                unvisited[node] = distance
+            distance = current_distance + heat_map[y][x]
+            if distance < result_map[node]:
+                result_map[node] = distance
+                heappush(queue, (distance, node))
 
-        visited[current_node] = unvisited[current_node]
-        del unvisited[current_node]
-        current_node = min(unvisited, key=unvisited.get)
+        visited.add(current_node)
 
-    return unvisited[current_node] - heat_map[1][1]
+    return min(v for k, v in result_map.items() if k[0] == (len(heat_map[0]) - 1, len(heat_map) - 1))
+
 def part_two(heat_map):
     """Ooooh, this is my second time using Dijkstra"""
-    # pad all sides with an inifinte value. This just
-    # makes indexing easier
-    heat_map.insert(0, [math.inf for n in range(len(heat_map[0]))])
-    heat_map.append([math.inf for n in range(len(heat_map[0]))])
-    heat_map = [[math.inf, *r, math.inf] for r in heat_map]
-
-    # dictionary keys need to include position,
-    # direction,
-    # and number of steps in a straight line to get there
-    unvisited = {}
+    result_map = {}
     for x in range(len(heat_map[0])):
         for y in range(len(heat_map)):
             for direction in "NESW":
                 for steps in range(1,11):
-                    unvisited[((x,y), direction, steps)] = math.inf
-                    #unvisited[((x,y), direction, steps)] = heat_map[1][1] if x == y == 1 else math.inf
-    unvisited[((1,1), "E", 1)] = heat_map[1][1]
-    unvisited[((1,1), "S", 1)] = heat_map[1][1]
+                    result_map[((x,y), direction, steps)] = math.inf
+                    
+    result_map[((0,0), "E", 1)] = 0
+    result_map[((0,0), "S", 1)] = 0
 
-    visited = {}
-    current_node = ((1,1), "E", 1)
+    visited = set()
+    queue = []
+    for n in result_map:
+        heappush(queue, (result_map[n], n))
+
     direction_coords = {"N": (0,-1), "E": (1,0), "S":(0,1), "W":(-1,0)}
     directions = {"N": "NEW", "E": "NES", "S": "ESW", "W": "NSW"}
 
-    while current_node[0] != (len(heat_map[0]) - 2, len(heat_map) - 2) or current_node[2] < 4:
-        # find adjacent nodes
+    while queue:
+        current_distance, current_node = heappop(queue)
+        
         adjacent_nodes = []
         for direction in directions[current_node[1]]:
             x = current_node[0][0] + direction_coords[direction][0]
             y = current_node[0][1] + direction_coords[direction][1]
             if current_node[1] != direction:
                 node = ((x,y), direction, 1)
-                if node in unvisited and current_node[2] >= 4:
+                if node not in visited and node in result_map and current_node[2] >= 4:
                     adjacent_nodes.append(node)
             else:
                 node = ((x,y), direction, current_node[2] + 1)
-                if node in unvisited and current_node[2] <= 9:
+                if node not in visited and current_node[2] <= 9 and node in result_map:
                     adjacent_nodes.append(node)
-
+        
         for node in adjacent_nodes:
             x, y = node[0]
-            distance = unvisited[current_node] + heat_map[y][x]
-            if distance < unvisited[node]:
-                unvisited[node] = distance
+            distance = current_distance + heat_map[y][x]
+            if distance < result_map[node]:
+                result_map[node] = distance
+                heappush(queue, (distance, node))
 
-        visited[current_node] = unvisited[current_node]
-        del unvisited[current_node]
-        current_node = min(unvisited, key=unvisited.get)
-
-    return unvisited[current_node] - heat_map[1][1]
+        visited.add(current_node)
+        
+    valid_nodes = {k: v for k, v in result_map.items() if k[0] == (len(heat_map[0]) - 1, len(heat_map) - 1)}
+    valid_nodes = {k: v for k, v in valid_nodes.items() if k[2] >= 4}
+    return min(valid_nodes.values())
 
 if __name__ == "__main__":
     with open("inputs/day_17.txt") as f:
@@ -108,3 +108,5 @@ if __name__ == "__main__":
         heat_map = [[int(x) for x in list(r)] for r in heat_map]
         min_heat_loss = find_minimum_heat_loss(heat_map)
         print(min_heat_loss, "< min heat loss")
+        ultra_heat_loss = part_two(heat_map)
+        print(ultra_heat_loss, "< ultra heat loss")
