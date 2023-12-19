95f7605221567cf06d82c8f8445bf29120bc7078
chris <c_duckworth@hotmail.com>
2023-12-19 11:45:24 +0000

    add test for sum accept function

--- tests/day_19_test.py
+++ tests/day_19_test.py
@@ -1,4 +1,4 @@
-from day_19 import get_info, eval_rule, eval_part
+from day_19 import get_info, eval_rule, eval_part, sum_accepted
 
 class TestGetInfo:
     def test_returns_parts(self):
@@ -111,3 +111,29 @@ hdj{m>838:A,pv}
         assert eval_part(parts[2], rules) == "A"
         assert eval_part(parts[3], rules) == "R"
         assert eval_part(parts[4], rules) == "A"
+        
+class TestSumAccepted:
+    def test_returns_sum_of_accepted_parts(self):
+        system = """px{a<2006:qkq,m>2090:A,rfg}
+pv{a>1716:R,A}
+lnx{m>1548:A,A}
+rfg{s<537:gd,x>2440:R,A}
+qs{s>3448:A,lnx}
+qkq{x<1416:A,crn}
+crn{x>2662:A,R}
+in{s<1351:px,qqz}
+qqz{s>2770:qs,m<1801:hdj,R}
+gd{a>3333:R,R}
+hdj{m>838:A,pv}
+
+{x=787,m=2655,a=1222,s=2876}
+{x=1679,m=44,a=2067,s=496}
+{x=2036,m=264,a=79,s=2244}
+{x=2461,m=1339,a=466,s=291}
+{x=2127,m=1623,a=2188,s=1013}"""
+        
+        rules, parts = get_info(system)
+
+        total = sum_accepted(parts, rules)
+
+        assert total == 19114
