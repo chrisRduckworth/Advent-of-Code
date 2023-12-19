6a41e80a688cc3df9810d63552454a4fc7587507
chris <c_duckworth@hotmail.com>
2023-12-19 14:07:50 +0000

    add tests for total possibilities function

--- tests/day_19_test.py
+++ tests/day_19_test.py
@@ -1,4 +1,4 @@
-from day_19 import get_info, eval_rule, eval_part, sum_accepted, find_new_range
+from day_19 import get_info, eval_rule, eval_part, sum_accepted, find_new_range, total_possibilities
 
 class TestGetInfo:
     def test_returns_parts(self):
@@ -221,3 +221,29 @@ class TestFindNewRange:
         
         assert new_range == 0
         assert pass_through == {"x":[3001,4000], "m":[1,4000], "a":[1,2005], "s":[1,4000]}
+
+class TestTotalPossibilities:
+    def test_returns_total_possibilities(self):
+        system = """px{a<2006:qkq,m>2090:A,rfg}
+pv{a>1716:R,A}
+lnx{m>1548:A,A}
+rfg{s<537:gd,x>2440:R,A}
+qs{s>3448:A,lnx}
+qkq{x<1416:A,crn}
+crn{x>2662:A,R}
+in{s<1351:px,qqz}
+qqz{s>2770:qs,m<1801:hdj,R}
+gd{a>3333:R,R}
+hdj{m>838:A,pv}
+
+{x=787,m=2655,a=1222,s=2876}
+{x=1679,m=44,a=2067,s=496}
+{x=2036,m=264,a=79,s=2244}
+{x=2461,m=1339,a=466,s=291}
+{x=2127,m=1623,a=2188,s=1013}"""
+        
+        rules, parts = get_info(system)
+
+        total = total_possibilities(rules)
+
+        assert total == 167409079868000
