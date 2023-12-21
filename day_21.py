c299d1b9f1a9e04a9e387cda03509960967dee6f
chris <c_duckworth@hotmail.com>
2023-12-21 13:59:48 +0000

    refactor to keep track of current distance to avoid overlap

--- day_21.py
+++ day_21.py
@@ -3,21 +3,36 @@ def total_steps(garden, steps):
     garden.append(list("#" * len(garden[0])))
     garden = [["#", *r, "#"] for r in garden]
     
-    y = [i for i, r in enumerate(garden) if "S" in r][0]
-    x = [i for i, c in enumerate(garden[y]) if "S" == c][0]
+    s_y = [i for i, r in enumerate(garden) if "S" in r][0]
+    s_x = [i for i, c in enumerate(garden[s_y]) if "S" == c][0]
 
     def flood_fill(x, y, current_steps):
-        if garden[y][x] == "#":
+        if garden[y][x] == "#" or current_steps > steps:
             return
-        if current_steps == steps:
-            garden[y][x] = "O"
-            return
+        if isinstance(garden[y][x], int):
+            if garden[y][x] <= current_steps:
+                return
+        garden[y][x] = current_steps
         flood_fill(x + 1, y, current_steps + 1)
         flood_fill(x - 1, y, current_steps + 1)
         flood_fill(x, y + 1, current_steps + 1)
         flood_fill(x, y - 1, current_steps + 1)
+        return 
         
-    flood_fill(x, y, 0)
+    flood_fill(s_x, s_y, 0)
 
-    return sum(r.count("O") for r in garden)
+    total = 0 
+    for j, y in enumerate(garden):
+        for i, x in enumerate(y):
+            if isinstance(x, int):
+                if x % 2 == steps % 2:
+                    total += 1
+    return total
     
+if __name__ == "__main__":
+    with open("inputs/day_21.txt") as f:
+        garden = f.read().splitlines()
+        garden = [list(r) for r in garden]
+        print("\n".join("".join(r) for r in garden))
+        steps = total_steps(garden, 64)
+        print(steps, "< total steps")
