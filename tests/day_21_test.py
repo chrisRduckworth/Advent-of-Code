ed86df0aaa14191229af21cbe09ad8d896f49402
chris <c_duckworth@hotmail.com>
2023-12-21 12:09:52 +0000

    add tests for total steps function

---
+++ tests/day_21_test.py
@@ -0,0 +1,28 @@
+from day_21 import total_steps
+
+class TestTotalSteps:
+    def test_returns_0_when_no_options(self):
+        garden = [["S"]]
+
+        steps = total_steps(garden, 10)
+        
+        assert steps == 0
+
+    def test_returns_number_of_steps(self):
+        garden = """...........
+.....###.#.
+.###.##..#.
+..#.#...#..
+....#.#....
+.##..S####.
+.##..#...#.
+.......##..
+.##.#.####.
+.##..##.##.
+...........""".splitlines()
+        garden = [list(r) for r in garden]
+        print(garden)
+
+        steps = total_steps(garden, 6)
+        
+        assert steps == 16
