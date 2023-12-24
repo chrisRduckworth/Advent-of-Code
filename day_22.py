d5a43de61a1a917371c5bb0e02068db854a48b02
chris <c_duckworth@hotmail.com>
2023-12-24 18:52:00 +0000

    add create_tower function

---
+++ day_22.py
@@ -0,0 +1,36 @@
+def create_tower(blocks):
+    # position (x,y,z) = tower[z][y][x]
+    max_x = max(block[1][0] for block in blocks) + 1
+    max_y = max(block[1][1] for block in blocks) + 1
+    max_z = max(block[1][2] for block in blocks) + 1
+
+    tower = [[[False] * max_x for y in range(max_y)] for z in range(max_z)]
+    
+    for i, block in enumerate(blocks):
+        if block[0][0] != block[1][0]:
+            # change is in x
+            start_x = block[0][0]
+            end_x = block[1][0] + 1
+            y, z = block[0][1:]
+            for x in range(start_x, end_x):
+                tower[z][y][x] = i
+        elif block[0][1] != block[1][1]:
+            # change is in y
+            start_y = block[0][1]
+            end_y = block[1][1] + 1
+            x, z = block[1][::2]
+            for y in range(start_y, end_y):
+                tower[z][y][x] = i
+        elif block[0][2] != block[1][2]:
+            # change is in z
+            start_z = block[0][2]
+            end_z = block[1][2] + 1
+            x, y = block[0][:2]
+            for z in range(start_z, end_z):
+                tower[z][y][x] = i
+        else:
+            # single length block
+            x, y, z = block[0]
+            tower[z][y][x] = i
+            
+    return tower
