d01b1b1fa1a063ab948d033fe30751ba0dd5a987
chris <c_duckworth@hotmail.com>
2024-01-03 18:33:45 +0000

    add tests for longest_path function

--- tests/day_23_test.py
+++ tests/day_23_test.py
@@ -1,5 +1,4 @@
-import pytest
-from day_23 import generate_graph 
+from day_23 import generate_graph, longest_path
 
 class TestGenerateGraph:
     def test_returns_dict_of_simple_graph(self):
@@ -7,8 +6,6 @@ class TestGenerateGraph:
 #..#
 ##.#""".splitlines()
         maze = [list(r) for r in maze]
-        
-        print(maze)
 
         graph = generate_graph(maze)
 
@@ -36,4 +33,63 @@ class TestGenerateGraph:
                 (1,1): {(1,0)}
                 }
 
+class TestLongestPath:
+    def test_returns_length_of_single_path(self):
+        maze = """#.##
+#..#
+##.#""".splitlines()
+        maze = [list(r) for r in maze]
 
+        graph = generate_graph(maze)
+
+        longest = longest_path(graph, (len(maze[0]) - 2, len(maze) - 1))
+
+        assert longest == 3
+
+    def test_returns_length_of_complex_path(self):
+        maze = """#.###
+#.>.#
+#.#v#
+#.>.#
+###.#""".splitlines()
+        maze = [list(r) for r in maze]
+
+        graph = generate_graph(maze)
+
+        longest = longest_path(graph, (len(maze[0]) - 2, len(maze) - 1))
+
+        assert longest == 6
+
+    def test_returns_length_for_big_maze(self):
+        maze = """#.#####################
+#.......#########...###
+#######.#########.#.###
+###.....#.>.>.###.#.###
+###v#####.#v#.###.#.###
+###.>...#.#.#.....#...#
+###v###.#.#.#########.#
+###...#.#.#.......#...#
+#####.#.#.#######.#.###
+#.....#.#.#.......#...#
+#.#####.#.#.#########v#
+#.#...#...#...###...>.#
+#.#.#v#######v###.###v#
+#...#.>.#...>.>.#.###.#
+#####v#.#.###v#.#.###.#
+#.....#...#...#.#.#...#
+#.#########.###.#.#.###
+#...###...#...#...#.###
+###.###.#.###v#####v###
+#...#...#.#.>.>.#.>.###
+#.###.###.#.###.#.#v###
+#.....###...###...#...#
+#####################.#
+""".splitlines()
+
+        maze = [list(r) for r in maze]
+
+        graph = generate_graph(maze)
+
+        longest = longest_path(graph, (len(maze[0]) - 2, len(maze) - 1))
+
+        assert longest == 94
