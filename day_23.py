dc210eb8708a78f934dd568106f904d85e304f9b
chris <c_duckworth@hotmail.com>
2024-01-03 18:16:25 +0000

    add generate graph function which returns dict of nodes with predecessors

---
+++ day_23.py
@@ -0,0 +1,29 @@
+def generate_graph(maze):
+    """generates a dictionary of nodes and their predocessors"""
+    maze = [["#"] * len(maze[0]), *maze, ["#"] * len(maze[0])]
+    graph = {}
+
+    def rec(node, prev):
+        n_x, n_y = node
+
+        if (n_x, n_y - 1) not in graph:
+            graph[(n_x, n_y - 1)] = set()
+
+        for dir in [(1,0, ">"), (-1,0, "<"), (0,1, "v"), (0,-1, "^")]:
+            x, y, slope = (n_x + dir[0], n_y + dir[1], dir[2])
+
+            if maze[y][x] in [".", slope] and (x,y) != prev:
+                graph[(n_x, n_y - 1)].add((x, y - 1))
+                rec((x, y), node)
+
+    rec((1,1), (1,0))
+
+    reversed = {}
+
+    for node, adj in graph.items():
+        for next in adj:
+            if next not in reversed:
+                reversed[next] = set()
+            reversed[next].add(node)
+
+    return reversed
